5741d746d1520c1aa1f6c2ac1c78b37ffa2cbee1
PraveenSS3B <praveenkumar66118@gmail.com>
2023-09-11 23:40:55 +0530

    added comments

--- MyArray.java
+++ MyArray.java
@@ -6,27 +6,49 @@ public class MyArray {
 
 	int n;
 
+	/**
+	 * Constructor that initializes the size of the array
+	 * @param n -> the size of the array.
+	 */
 	public MyArray(int n) {
 		this.n = n;
 		arr = new int[n];
 	}
 
+	/**
+	 * 
+	 * @param index inserts element at the specified index
+	 * @param data  that goes into index.
+	 */
 	public void insert(int index, int data) {
 		if (index <= n - 1) {
 			arr[index] = data;
 		}
 	}
 
+	/**
+	 * 
+	 * @param data that adds the element at end of array
+	 */
 	public void addRear(int data) {
 		arr[n - 1] = data;
 	}
 
+	/**
+	 * 
+	 * @param this method removes the element at this specified index
+	 */
 	public void remove(int index) {
 		if (index <= n - 1) {
 			arr[index] = 0;
 		}
 	}
 
+	/**
+	 * 
+	 * @param specify index to get the data
+	 * @return gets the data at the specified index
+	 */
 	public int get(int index) {
 		if (index <= n - 1)
 			return arr[index];
